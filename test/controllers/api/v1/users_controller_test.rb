ab97dcfdf6bbadb3fb85e9b1b058c09462c4a773
Eduardo Mendez <joseeduardomendezb@gmail.com>
2021-07-29 00:25:20 -0700

    Add products relationship to user#show

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -10,43 +10,45 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     # Test to ensure response contains the correct email
-    json_response = JSON.parse(response.body)
-    assert_equal @user.email, json_response["data"]["attributes"]["email"]
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    assert_equal @user.email, json_response.dig(:data, :attributes, :email)
+    assert_equal @user.products.first.id.to_s, json_response.dig(:data, :relationships, :products, :data, 0, :id)
+    assert_equal @user.products.first.title, json_response.dig(:included, 0, :attributes, :title)
   end
 
   test "should create user" do
     assert_difference("User.count") do
-      post api_v1_users_url, params: {user: {email: "test@test.org", password: "123456"}}, as: :json
+      post api_v1_users_url, params: { user: { email: "test@test.org", password: "123456" } }, as: :json
     end
     assert_response :created
   end
 
   test "should not create user with taken email" do
     assert_no_difference("User.count") do
-      post api_v1_users_url, params: {user: {email: @user.email, password: "123456"}}, as: :json
+      post api_v1_users_url, params: { user: { email: @user.email, password: "123456" } }, as: :json
     end
     assert_response :unprocessable_entity
   end
 
   test "should update user" do
     patch api_v1_user_url(@user),
-      params: {user: {email: @user.email}},
-      headers: {Authorization: JsonWebToken.encode(user_id: @user.id)},
+      params: { user: { email: @user.email } },
+      headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
       as: :json
     assert_response :success
   end
 
   test "should forbid update user" do
     patch api_v1_user_url(@user),
-      params: {user: {email: @user.email}},
+      params: { user: { email: @user.email } },
       as: :json
     assert_response :forbidden
   end
 
   test "should not update the user when invalid params are sent" do
     patch api_v1_user_url(@user),
-      params: {user: {email: "bad_email", password: "123456"}},
-      headers: {Authorization: JsonWebToken.encode(user_id: @user.id)},
+      params: { user: { email: "bad_email", password: "123456" } },
+      headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
       as: :json
     assert_response :unprocessable_entity
   end
@@ -54,7 +56,7 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   test "should destroy user" do
     assert_difference("User.count", -1) do
       delete api_v1_user_url(@user),
-        headers: {Authorization: JsonWebToken.encode(user_id: @user.id)},
+        headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
         as: :json
     end
     assert_response :no_content
